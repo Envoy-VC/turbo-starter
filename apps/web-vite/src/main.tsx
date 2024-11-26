32ae6a5fb75b2235061eebcbccefcb2a879b08cd
Vedant Chainani <vedantchainani1084@gmail.com>
2024-11-26 18:22:15 +0530

    chore: add tailwind

--- apps/web-vite/src/main.tsx
+++ apps/web-vite/src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 import { routeTree } from './route-tree.gen';
+import './styles/globals.css';
 
 // Set up a Router instance
 const router = createRouter({
@@ -18,9 +18,10 @@ declare module '@tanstack/react-router' {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- safe-assertion
 const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
+  const root = createRoot(rootElement);
   root.render(<RouterProvider router={router} />);
 }
