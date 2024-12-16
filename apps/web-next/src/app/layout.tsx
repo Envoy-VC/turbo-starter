fbe1930a01353516508976a28929429177a75c77
Vedant Chainani <vedantchainani1084@gmail.com>
2024-12-17 00:18:01 +0530

    chore: remove shared ui and add ui to both templates

--- apps/web-next/src/app/layout.tsx
+++ apps/web-next/src/app/layout.tsx
@@ -1,8 +1,8 @@
-import '@repo/ui/globals.css';
 import { GeistSans } from 'geist/font/sans';
 import { type Metadata } from 'next';
-import '~/styles/globals.css';
 import { TRPCReactProvider } from '~/trpc/react';
+
+import '../styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Create T3 App',
